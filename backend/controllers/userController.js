44b0d7da421ce43d3afae742808cbdf0e495ba4f
Sallehhuddin95 <62774625+Sallehhuddin95@users.noreply.github.com>
2022-07-10 21:45:36 +0800

    add delete and update request
    - delete and update request for backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,16 +2,16 @@ const asyncHandler = require("express-async-handler");
 
 const User = require("../model/userModel");
 
-// @ desc get goals
-// @ route Get /api/goals
+// @ desc get users
+// @ route Get /api/employees
 const getUsers = asyncHandler(async (req, res) => {
   const users = await User.find();
 
   res.status(200).json(users);
 });
 
-// @ desc set goals
-// @ route Post /api/goals
+// @ desc set users
+// @ route Post /api/employees
 const setUsers = asyncHandler(async (req, res) => {
   if (!req.body.userID) {
     throw new Error("Please add a user id field");
@@ -36,8 +36,8 @@ const setUsers = asyncHandler(async (req, res) => {
   res.status(200).json(user);
 });
 
-// @ desc update goals
-// @ route Put /api/goals/:id
+// @ desc update users
+// @ route Put /api/employees/id
 const updateUsers = asyncHandler(async (req, res) => {
   const user = await User.findById(req.params.id);
 
@@ -46,32 +46,20 @@ const updateUsers = asyncHandler(async (req, res) => {
     throw new Error("User not found");
   }
 
-  //Check for user
-  if (!req.id) {
-    res.status(401);
-    throw new Error("User not found");
-  }
-
-  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+  const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
 
-  res.status(200).json(updatedGoal);
+  res.status(200).json(updatedUser);
 });
 
-// @ desc Delete goals
-// @ route Delete /api/goal/:id
+// @ desc Delete users
+// @ route Delete /api/employees/id
 const deleteUser = asyncHandler(async (req, res) => {
-  const user = await Goal.findById(req.params.id);
+  const user = await User.findById(req.params.id);
 
   if (!user) {
     res.status(400);
-    throw new Error("User not found");
-  }
-
-  //Check for user
-  if (!req.user) {
-    res.status(401);
     throw new Error("User not found");
   }
 
