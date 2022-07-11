843fe596f8ccc95561cf81fc94f320ef12521ff5
Sallehhuddin95 <62774625+Sallehhuddin95@users.noreply.github.com>
2022-07-11 23:41:27 +0800

    Make sidebar responsive
    Make sidebar responsive

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -11,8 +11,6 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import FileUploader from "./FileUploader";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 
 const drawerWidth = 240;
 const profilePic = require("../images/avatar_profile_icon.png");
@@ -30,73 +28,16 @@ export default function Sidebar(props) {
         anchor="left"
         sx={{
           width: drawerWidth,
-          display: { xs: "none", sm: "block" },
-          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
         }}
         // open
       >
         {/* <Toolbar /> */}
 
-        <Card sx={{ maxWidth: 345 }} className="profile-div">
-          <CardMedia
-            className="profile-img"
-            component="img"
-            height="140"
-            image={profilePic}
-            alt="profile picture"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Long User Name
-            </Typography>
-          </CardContent>
-        </Card>
-
-        <List>
-          {[
-            "Upload Data",
-            "Function 2",
-            "Function 3",
-            "Function 4",
-            "Function 5",
-          ].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              {index === 0 ? (
-                <FileUploader />
-              ) : (
-                <ListItemButton>
-                  <ListItemIcon>
-                    <FaCloudDownloadAlt />
-                  </ListItemIcon>
-                  <ListItemText primary={text} />
-                </ListItemButton>
-              )}
-            </ListItem>
-          ))}
-        </List>
-      </Drawer>
-      <IconButton
-        color="inherit"
-        aria-label="open drawer"
-        edge="start"
-        onClick={handleDrawerToggle}
-        sx={{ mr: 2, display: { sm: "none" } }}
-        className="menu-btn"
-      >
-        <MenuIcon />
-      </IconButton>
-      <Drawer
-        variant="temporary"
-        open={mobileOpen}
-        onClose={handleDrawerToggle}
-        ModalProps={{
-          keepMounted: true, // Better open performance on mobile.
-        }}
-        sx={{
-          display: { xs: "block", sm: "none" },
-          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
-        }}
-      >
         <Card sx={{ maxWidth: 345 }} className="profile-div">
           <CardMedia
             className="profile-img"
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,50 +1,172 @@
-import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { getEmployees, reset } from "../features/employee/employeeSlice";
-import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Toolbar from "@mui/material/Toolbar";
+// import React, { useEffect } from "react";
+// import { useSelector, useDispatch } from "react-redux";
+// import { getEmployees, reset } from "../features/employee/employeeSlice";
+// import Box from "@mui/material/Box";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import Toolbar from "@mui/material/Toolbar";
 import Sidebar from "../components/Sidebar";
 import EmployeeTable from "../components/EmployeeTable";
-// import FilterBox from "../components/FilterBox";
+// // import FilterBox from "../components/FilterBox";
+
+// const drawerWidth = 240;
+
+// export default function Dashboard() {
+//   const dispatch = useDispatch();
+
+//   const { employees, isLoading, isError, message } = useSelector(
+//     (state) => state.employees
+//   );
+
+//   useEffect(() => {
+//     if (isError) {
+//       console.log(message);
+//     }
+
+//     // get goals from backend and put in the goals variable
+//     console.log("get employees");
+//     dispatch(getEmployees());
+
+//     //Reset the goals when we leave the page
+//     return () => {
+//       dispatch(reset());
+//     };
+//   }, [isError, dispatch]);
+
+//   return (
+//     <Box sx={{ display: "flex" }}>
+//       <CssBaseline />
+//       <Sidebar />
+//       <Box
+//         component="main"
+//         sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+//       >
+//         {/* <Toolbar /> */}
+//         {/* <FilterBox /> */}
+//         <h2>Employees</h2>
+//         <EmployeeTable />
+//       </Box>
+//     </Box>
+//   );
+// }
+
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import MailIcon from "@mui/icons-material/Mail";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 
 const drawerWidth = 240;
 
-export default function Dashboard() {
-  const dispatch = useDispatch();
+function ResponsiveDrawer(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const { employees, isLoading, isError, message } = useSelector(
-    (state) => state.employees
-  );
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
-  useEffect(() => {
-    if (isError) {
-      console.log(message);
-    }
-
-    // get goals from backend and put in the goals variable
-    console.log("get employees");
-    dispatch(getEmployees());
-
-    //Reset the goals when we leave the page
-    return () => {
-      dispatch(reset());
-    };
-  }, [isError, dispatch]);
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
 
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <Sidebar />
+      <AppBar
+        position="fixed"
+        sx={{
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          {/* <Typography variant="h6" noWrap component="div">
+            Responsive drawer
+          </Typography> */}
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          <Sidebar />
+        </Drawer>
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          <Sidebar />
+          {/* {drawer} */}
+        </Drawer>
+      </Box>
       <Box
         component="main"
-        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
       >
-        {/* <Toolbar /> */}
-        {/* <FilterBox /> */}
+        <Toolbar />
         <h2>Employees</h2>
         <EmployeeTable />
       </Box>
     </Box>
   );
 }
+
+ResponsiveDrawer.propTypes = {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+
+export default ResponsiveDrawer;
